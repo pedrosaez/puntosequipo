aa0e650b30c413b6186a44d6d1e57ab1ad111723
pedro saez <pedrosaez02@gmail.com>
2022-07-21 14:56:17 -0300

    agregando alert

--- js/main.js
+++ js/main.js
@@ -6,16 +6,16 @@ var empates = parseInt(prompt("Informe los empates de su equipo"));
 
 var puntosTotal = (victorias * 3 + empates);
 
-console.log("El total de puntos de " + nombreEquipo + " es: " + puntosTotal);
+alert("El total de puntos de " + nombreEquipo + " es: " + puntosTotal);
 
 if (puntosTotal > 28) {
-console.log("El equipo está mejor que el año pasado");
+alert("El equipo está mejor que el año pasado");
 }
 
 if (puntosTotal < 28) {
-console.log("El equipo está peor que el año pasado");
+alert("El equipo está peor que el año pasado");
 }
 
 if (puntosTotal == 28) {
-console.log("El equipo está igual que el año pasado");
+alert("El equipo está igual que el año pasado");
 }
